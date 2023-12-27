2cf97959f30cbb43d6f23ef0fdb9c32082a096af
robert.dimitrovski@gmail.com <robert.dimitrovski@gmail.com>
2023-12-27 16:11:57 +0100

    Ieee 9bus modified

--- DQSYM/01_TestCases/01_IEEE_9bus/IEEE_9bus_calc.m
+++ DQSYM/01_TestCases/01_IEEE_9bus/IEEE_9bus_calc.m
@@ -29,52 +29,53 @@ file = 'IEEE_9bus.xlsx';
 [SYS, rlc] = fNET2SS(file);
 
 run("IEEE_9bus")
+sim("IEEE_9bus")
 
-LF = power_loadflow('-v2','IEEE_9bus','solve')
-% 
-% 
-% 
-% 
+% LF = power_loadflow('-v2','IEEE_9bus','solve')
 % % 
 % % 
-g = Simulink.Mask.get('IEEE_9bus/M1');
-vars_g = g.getWorkspaceVariables;
-SM1 = vars_g(21).Value;
-% 
-g = Simulink.Mask.get('IEEE_9bus/M2');
-vars_g = g.getWorkspaceVariables;
-SM2 = vars_g(21).Value;
-% 
-g = Simulink.Mask.get('IEEE_9bus/M3');
-vars_g = g.getWorkspaceVariables;
-SM3 = vars_g(21).Value;
 % % 
-% [real(100*LF.sm(2).S/Sn), imag(100*LF.sm(2).S/Sn), abs(LF.bus(1).Vbus), LF.sm(2).Vf, deg2rad(LF.sm(2).th0)]
+% % 
+% % % 
+% % % 
+% g = Simulink.Mask.get('IEEE_9bus/M1');
+% vars_g = g.getWorkspaceVariables;
+% SM1 = vars_g(21).Value;
+% % 
+% g = Simulink.Mask.get('IEEE_9bus/M2');
+% vars_g = g.getWorkspaceVariables;
+% SM2 = vars_g(21).Value;
+% % 
+% g = Simulink.Mask.get('IEEE_9bus/M3');
+% vars_g = g.getWorkspaceVariables;
+% SM3 = vars_g(21).Value;
+% % % 
+% % [real(100*LF.sm(2).S/Sn), imag(100*LF.sm(2).S/Sn), abs(LF.bus(1).Vbus), LF.sm(2).Vf, deg2rad(LF.sm(2).th0)]
+% % 
+% % 
+% % 
+% % 
+% 
+% SMRD2 = fCalcMacParam(2.8544e-3, ...
+%                     [200E6  18e3   60 ], ...
+%                     [ 1.305, 0.296, 0.252, 0.474, 0.243, 0.18 ], ...
+%                     [ 1.01, 0.053, 0.0513 ], ...
+%                     [0.8250    0.2219    1.0000    1.5762   deg2rad(6.37)], ...
+%                     [ 3.7 0  32], Ts)
 % 
 % 
 % 
+% SMRD3 = fCalcMacParam(2.8544e-3, ...
+%                     [200E6  13.8e3   60 ], ...
+%                     [ 1.305, 0.296, 0.252, 0.474, 0.243, 0.18 ], ...
+%                     [ 1.01, 0.053, 0.0513 ], ...
+%                     [0.4250    0.1355    1.0000    1.2604  deg2rad(-0.3643)], ...
+%                     [ 3.7 0  32], Ts)
 % 
-
-SMRD2 = fCalcMacParam(2.8544e-3, ...
-                    [200E6  18e3   60 ], ...
-                    [ 1.305, 0.296, 0.252, 0.474, 0.243, 0.18 ], ...
-                    [ 1.01, 0.053, 0.0513 ], ...
-                    [0.8250    0.2219    1.0000    1.5762   deg2rad(6.37)], ...
-                    [ 3.7 0  32], Ts)
-
-
-
-SMRD3 = fCalcMacParam(2.8544e-3, ...
-                    [200E6  13.8e3   60 ], ...
-                    [ 1.305, 0.296, 0.252, 0.474, 0.243, 0.18 ], ...
-                    [ 1.01, 0.053, 0.0513 ], ...
-                    [0.4250    0.1355    1.0000    1.2604  deg2rad(-0.3643)], ...
-                    [ 3.7 0  32], Ts)
-
-[SM2.phiqd0'; SMRD2.phidq0']
-[SM2.iqd0'; SMRD2.idq0']
-
-[SM3.phiqd0'; SMRD3.phidq0']
+% [SM2.phiqd0'; SMRD2.phidq0']
+% [SM2.iqd0'; SMRD2.idq0']
+% 
+% [SM3.phiqd0'; SMRD3.phidq0']
 %[SM3.iqd0'; SMRD3.idq0']
 
 % % % 
