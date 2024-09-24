831ebf29a9880ea54c6cef899fc5e5f1e61c8edb
HVDC/AC Control <131252855+control-protection-grids-tudelft@users.noreply.github.com>
2024-09-24 13:22:33 +0200

    Admittance added

---
+++ Harmony_Cpp/Harmony/Admittance.cpp
@@ -0,0 +1,41 @@
+﻿#include "Admittance.h"
+
+#include <cmath>
+using namespace std;
+using namespace SymEngine;
+
+Admittance::Admittance(int pins, vector<RCP<Symbol>> values) {
+    Element::input_pins = pins;
+    Element::output_pins = pins;
+    admittance = RCP<Symbol>[pins][pins];
+    if (values.capacity() != 0)  // if there are entries
+    {
+        if (pins != 0) // if there are element connections check further
+        {
+            switch (values.capacity()) {
+            case 1: {
+                for (int i = 0; i < pins; i++)
+                    admittance[i][i] = values[0];
+            }
+            case pins: {
+                for (int i = 0; i < pins; i++)
+                    admittance[i][i] = values[i];
+            }
+            case pow(pins, 2): {
+                for (int i = 0; i < pins; i++)
+                    for (int j = 0; j < pins; j++)
+                        admittance[i][j] = values[i + j];
+            }
+            default:
+                throw invalid_argument("Invalid number of admittance vector entries: " + values.capacity());
+            }
+        }
+        else
+            throw invalid_argument("Invalid number of pins, must be greater than 0!");
+    }
+}
+
+
+            
+
+           
---
+++ Harmony_Cpp/Harmony/Admittance.h
@@ -0,0 +1,38 @@
+﻿#ifndef ADMITTANCE_H
+#define ADMITTANCE_H
+
+#include "Element.h"
+
+#include <symengine/expression.h>
+#include <symengine/symbol.h>
+#include <symengine/add.h>
+#include <symengine/mul.h>
+#include <symengine/pow.h>
+#include <symengine/real_double.h>
+#include <symengine/eval.h>
+#include <symengine/functions.h>
+#include <symengine/complex.h>
+#include <symengine/complex_double.h>
+
+
+/*
+Creates admittance with specified number of input/output pins `pins`. The admittance expression
+ `exp` has to be given in Ω and can have both numerical and symbolic value (example: `z = s-2`).
+Depending on the provided vector of admittance values, we differ three cases. If the number of 
+element pins is greater than 1, admittance can be represented with vector with one, `pins` or 
+`pins × pins` number of elements. Namely
+-In the case of 1×1 vector, admittance has only one value. Then this value is given to all diagonal
+admittance entries.
+-In the case of `pins` elements, they are representing diagonal entiries of admittance.
+-In case of `pins x pins` elements, they are representing all matrix of admittance.
+*/
+
+class Admittance : public Element {
+public:
+	Admittance(int pins, std::vector<RCP<Symbol>> admittance);
+	~Admittance() {};
+private:
+	RCP<Symbol>** admittance;
+};
+
+#endif
