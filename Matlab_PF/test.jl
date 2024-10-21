575b8459b885bc1ed8be7c21539524f0d5450607
h.li <160036988+HLi199305-ee@users.noreply.github.com>
2024-10-21 23:54:40 +0200

    Delete Matlab_PF/test.jl

--- Matlab_PF/test.jl
+++
@@ -1,8 +0,0 @@
-using JuMP, Ipopt, Printf
- include("create_ac.jl") # define interconnceted ac grids
- include("create_dc.jl") # define multi-terminal dc grid
- include("makeYbus.jl") # calculate Bus Admittance Matrix
- include("params_dc.jl") # obtain ac grid parameters 
- include("params_ac.jl") # obtain dc grid parameters 
- include("acmtdcpf.jl") # run ac/dc opf 
- acmtdcpf("ac5ac9","mtdc3slack_a")
