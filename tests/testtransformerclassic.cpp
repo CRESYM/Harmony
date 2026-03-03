10b16b2ee89474ba6c906ccfdbd8e261fb582e66
Yasel Quintero <y.i.quinterolares@tudelft.nl>
2026-03-03 16:09:30 +0100

    Fix tests for macOS

--- tests/testtransformerclassic.cpp
+++ tests/testtransformerclassic.cpp
@@ -6,26 +6,28 @@ class TestTransformerClassic : public testing::Test {};
 // Test constructor 
 TEST_F(TestTransformerClassic, TestConstructor) {
 	std::vector<double> transformer_values = { 1.0, 1.0e-3, 2.0, 2.0e-3, 0.5e-3 }; // R_primary, L_primary, R_secondary, L_secondary, Mutual inductance
-	Transformer_classic* transformer = new Transformer_classic("T1", 1, transformer_values);
+	Transformer_classic transformer = Transformer_classic("T1", "AC1", 1, transformer_values);
 
-	EXPECT_EQ(transformer->getResistance(0), 1.0);
-	EXPECT_EQ(transformer->getInductance(0), 1.0e-3);
-	EXPECT_EQ(transformer->getResistance(1), 2.0);
-	EXPECT_EQ(transformer->getInductance(1), 2.0e-3);
-	EXPECT_EQ(transformer->getMutualInductance(), 0.5e-3);
+	EXPECT_EQ(transformer.getResistance(0), 1.0);
+	EXPECT_EQ(transformer.getInductance(0), 1.0e-3);
+	EXPECT_EQ(transformer.getResistance(1), 2.0);
+	EXPECT_EQ(transformer.getInductance(1), 2.0e-3);
+	EXPECT_EQ(transformer.getMutualInductance(), 0.5e-3);
 }
 
 // Test constructor 
 TEST_F(TestTransformerClassic, TestYMatrix) {
 	std::vector<double> transformer_values = { 1.0, 1.0e-3, 2.0, 2.0e-3, 0.5e-3 }; // R_primary, L_primary, R_secondary, L_secondary, Mutual inductance
-	Transformer_classic* transformer = new Transformer_classic("T1", 1, transformer_values);
+	Transformer_classic transformer = Transformer_classic("T1", "AC1", 1, transformer_values);
 	
-	MatrixXcd y = transformer->compute_y_parameters_num(1500);
+	auto ytmp = transformer.compute_y_parameters(1500);
+	MatrixXcd y(2,2);
+	y << ytmp[0][0], ytmp[0][1], ytmp[1][0], ytmp[1][1];
 	MatrixXcd yexpected(2,2);
 	std::complex<double> Zp = 1.0 + std::complex<double>(0, 1.0e-3 * 1500);
 	std::complex<double> Zs = 2.0 + std::complex<double>(0, 2.0e-3 * 1500);
 	std::complex<double> Zm = std::complex<double>(0, 0.5e-3 * 1500);
-	std::complex<double> D = Zp*Zs - pow(M,2);
+	std::complex<double> D = Zp*Zs - pow(M_PI,2);
 	yexpected << Zs/D, -Zm/D, -Zm/D, Zp/D;
 	EXPECT_TRUE(y.isApprox(yexpected, 1e-9));
 }
